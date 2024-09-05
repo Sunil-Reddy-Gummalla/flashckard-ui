32406fe030ea67da7f08f22b3413674e4f4b6aa1
Sunil-Reddy-Gummalla <gummalla.sunilreddy@gmail.com>
2024-09-05 18:53:46 +0400

    testing logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ export function Header({ fetchCards }) {
     const handleLogout = () => {
         localStorage.removeItem('access_token');
         localStorage.removeItem('refresh_token');
-        window.location.href = '/login';
+        window.location.href = '/';
     };
 
     return (
